fe9f2b6543c14692124f7b45ded5a8e22fb29cb5
tdenis29 <tdenis29@outlook.com>
2021-09-21 13:00:19 -0700

    Update nav.php

--- template-parts/header/nav.php
+++ template-parts/header/nav.php
@@ -1,75 +1,82 @@
-
-<?php 
+<?php
 /**
- * Nav Component
- * @package Canopus
+ * Header Navigation template.
+ *
+ * @package Aquila
  */
-$menu_class = \CANOPUS_THEME\Inc\Menus::get_instance();
-$header_menu_id = $menu_class -> get_menu_id( 'canopus-header-menu' );
 
+$menu_class = \CANOPUS_THEME\Inc\Menus::get_instance();
+$header_menu_id = $menu_class->get_menu_id( 'canopus-header-menu' );
 $header_menus = wp_get_nav_menu_items( $header_menu_id );
 
 ?>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+<nav class="navbar navbar-expand-lg navbar-light bg-light d-spacebetween">
+	<?php
+	if ( function_exists( 'the_custom_logo' ) ) {
+		the_custom_logo();
+	}
+	?>
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+	</button>
 
-  <div class="collapse navbar-collapse justify-content-between" id="navbarTogglerDemo02">
-    <?php 
-    if (! empty($header_menus) && is_array($header_menus) ) {
-    ?>
-      <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-        <?php 
-           foreach ($header_menus as $menu_item) {
-             if ( ! $menu_item->$menu_item_parent ) {
-              
-              $child_menu_items = $menu_class-> get_child_menu_items($header_menus, $menu_item->ID);
-              $has_children = ! empty( $child_menu_items ) && is_array($child_menu_items);
-             
-              if (! $has_children) {
-          
-                ?>
-                
-                <li class="nav-item">
-                  <a class="nav-link" href="<?php echo esc_url($menu_item->url )?>"> <?php echo esc_html($menu_item->title)?> </a>
-                </li>
-                <?php
-              } else {
-                ?>
-                   <li class="nav-item dropdown">
-                   <a class="nav-link dropdown-toggle" href="<?php echo esc_url($menu_item->url )?>" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <?php echo esc_html($menu_item->title)?>
-                   </a>
-                   <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                     <?php 
-                     foreach ($child_menu_items as $child_menu_item) {
-                       
-                       ?>
-                       <a class="dropdown-item" href="<?php echo esc_url($child_menu_item->url )?>"><?php echo esc_html($child_menu_item->title)?></a>
-                       <?php 
-                     }
-                     ?>
-                   </div>
-                 </li>
-                 <?php
-              }
-             }
-           }
-        ?>
-      </ul>
-    <?php 
-    }
-    ?>
-    <form class="form-inline my-2 my-lg-0 d-flex">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
+	<div class="collapse navbar-collapse" id="navbarSupportedContent">
+		<?php
+		if ( ! empty( $header_menus ) && is_array( $header_menus ) ) {
+			?>
+			<ul class="navbar-nav mr-auto">
+				<?php
+					foreach ( $header_menus as $menu_item ) {
+						if ( ! $menu_item->menu_item_parent ) {
+
+							$child_menu_items = $menu_class->get_child_menu_items( $header_menus, $menu_item->ID );
+							$has_children = ! empty( $child_menu_items ) && is_array( $child_menu_items );
+							$has_sub_menu_class = ! empty( $has_children ) ? 'has-submenu' : '';
+
+							if ( ! $has_children ) {
+								?>
+								<li class="nav-item">
+									<a class="nav-link" href="<?php echo esc_url( $menu_item->url ); ?>">
+										<?php echo esc_html( $menu_item->title ); ?>
+									</a>
+								</li>
+								<?php
+							} else {
+								?>
+								<li class="nav-item dropdown">
+									<a class="nav-link dropdown-toggle" href="<?php echo esc_url( $menu_item->url ); ?>" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+										<?php echo esc_html( $menu_item->title ); ?>
+									</a>
+									<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+										<?php
+											foreach ( $child_menu_items as $child_menu_item ) {
+												?>
+												<a class="dropdown-item" href="<?php echo esc_url( $child_menu_item->url ); ?>">
+													<?php echo esc_html( $child_menu_item->title ); ?>
+												</a>
+												<?php
+											}
+										?>
+									</div>
+								</li>
+								<?php
+							}
+							?>
+							<?php
+						}
+					}
+				?>
+			</ul>
+			<?php
+		}
+		?>
+		<form class="form-inline my-2 my-lg-0">
+			<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+			<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+		</form>
+	</div>
 </nav>
-
 <!-- 
 <li class="nav-item">
             <a class="nav-link" href="#">Link</a>
